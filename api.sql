c7096ce90b91ce190fed616cc3e8f9e17af89b08
Gerrit Balindt <gerrit.balindt@thuenen.de>
2024-05-15 10:07:23 +0200

    chore: Grant usage on schema api to anon and update PostgreSQL port in docker-compose.yml

--- api.sql
+++ api.sql
@@ -1,5 +1,8 @@
 CREATE SCHEMA api AUTHORIZATION postgres;
 
+-- GRANT USAGE ON SCHEMA api TO anon;
+
+
 -- Propergate schema names to the schema_names view
 CREATE VIEW api.schema_names AS
 SELECT schema_name
