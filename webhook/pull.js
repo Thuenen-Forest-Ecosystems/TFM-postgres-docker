0bdbb16e3e5770b6ba64fb8f4b10c9db017ce4b9
Gerrit Balindt <gerrit.balindt@thuenen.de>
2024-06-06 11:30:07 +0200

    pull to test

--- webhook/pull.js
+++ webhook/pull.js
@@ -4,40 +4,39 @@ const{ execSync } = require('child_process');
 
 const { v2 } = require('docker-compose');
 
-
-const options = {
-  baseDir: path.resolve(__dirname, '../'),
-  binary: 'git',
-  maxConcurrentProcesses: 6,
-  trimmed: false,
-};
-
-const git = simpleGit(options);
+function pullRepository(gitPath, options, cb) {
+    const git = simpleGit(path.resolve(__dirname, gitPath), { binary: 'git' });
+    return git.pull(cb);
+}
 
 function pull_structure_and_data() {
-    let git = simpleGit(path.resolve(__dirname, '../ci2027-db-structure'), { binary: 'git' });
-    git.pull((err, update) => {
+    pullRepository('../', {binary: 'git'}, (err, update) => {
         if (err) {
-            console.log('Error: ', err);
+          console.log('Error: ', err);
         } else {
-            console.log('Update: ', update);
-        }
-    });
-    git = simpleGit(path.resolve(__dirname, '../ci2027-db-data'), { binary: 'git' });
-    git.pull((err, update) => {
-        if (err) {
-            console.log('Error: ', err);
-        } else {
-            console.log('Update: ', update);
-        }
-    });
-}
-function pull() {
-    git.pull((err, update) => {
-        if (err) {
-            console.log('Error: ', err);
-        } else {
-            console.log('Update: ', update);
+          pullRepository('../ci2027-db-structure', {
+            binary: 'git',
+            config: [
+              `http.extraHeader=Authorization: Bearer ${process.env.gitlabStructureToken}`
+            ]
+          }, (err, update) => {
+            if (err) {
+              console.log('Error: ', err);
+            } else {
+              pullRepository('../ci2027-db-data', {
+                binary: 'git',
+                config: [
+                  `http.extraHeader=Authorization: Bearer ${process.env.gitlabDataToken}`
+                ]
+              }, (err, update) => {
+                if (err) {
+                  console.log('Error: ', err);
+                } else {
+                  execSync('npm run start')
+                }
+              });
+            }
+          });
         }
     });
 }
